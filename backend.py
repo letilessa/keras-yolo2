83f728869bc23f1b90b1f0f666b1d5ad06d7fcbd
Letícia Lessa Teixeira <36640512+letilessa@users.noreply.github.com>
2018-08-14 15:42:29 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ from keras.applications.resnet50 import ResNet50
 FULL_YOLO_BACKEND_PATH  = "full_yolo_backend.h5"   # should be hosted on a server
 TINY_YOLO_BACKEND_PATH  = "tiny_yolo_backend.h5"   # should be hosted on a server
 SQUEEZENET_BACKEND_PATH = "squeezenet_backend.h5"  # should be hosted on a server
-MOBILENET_BACKEND_PATH  = "mobilenet_backend.h5"   # should be hosted on a server
+MOBILENET_BACKEND_PATH  = "/media/eHD/leticia/mobilenet_backend.h5"   # should be hosted on a server
 INCEPTION3_BACKEND_PATH = "inception_backend.h5"   # should be hosted on a server
 VGG16_BACKEND_PATH      = "vgg16_backend.h5"       # should be hosted on a server
 RESNET50_BACKEND_PATH   = "resnet50_backend.h5"    # should be hosted on a server
